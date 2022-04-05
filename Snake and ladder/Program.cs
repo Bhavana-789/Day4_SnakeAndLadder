541724dcc1cd16e822563dfe0e93912636c8c14a
Bhavana <bhavananerlakota@gmail.com>
2022-04-06 04:17:23 +0530

    Added UC1 Start position

--- Snake and ladder/Program.cs
+++ Snake and ladder/Program.cs
@@ -7,6 +7,12 @@ namespace Snake_and_ladder
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Snake and Ladder program");
+            Console.WriteLine("Welcome to Snake and ladder program");
+            int player = 1;
+            int StartingPosition = 0;
+            Console.WriteLine("No of player=" + player);
+            Console.WriteLine("Position at start:" + StartingPosition);
+            Console.ReadLine();
         }
     }
 }
