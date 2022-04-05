f2c7ed70edc638ad698d8f2f852fe7c7e0e29e7c
Bhavana <bhavananerlakota@gmail.com>
2022-04-06 04:28:37 +0530

    Added UC4 Repeat till win

--- Snake and ladder/Program.cs
+++ Snake and ladder/Program.cs
@@ -7,32 +7,53 @@ namespace Snake_and_ladder
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Snake and Ladder program");
-            //UC3
-            Console.WriteLine("Welcome to UC3 program");
-            Console.WriteLine("Welcome to UC3 program");
-            int Noplay = 0;
-            int ladder = 1;
-            int snake = 2;
-            Random random = new Random();
-            int randomCheck = random.Next(1, 3);
-            Console.WriteLine("Checking for status" + randomCheck);
-            if (randomCheck == ladder)
+            //UC4
+            Console.WriteLine("Welcome to UC4 program");
+            int ladder = 1, ldice;
+            int snake = 2, sdice;
+            int win = 100;
+            int count = 0;
+            Console.WriteLine("Welcome to UC_4");
+            while (count <= win)
             {
-                Random random2 = new Random();
-                int Diceroll = random2.Next(1, 7);
-                Console.WriteLine("You have moved forward by:" + Diceroll);
-            }
-            else if (randomCheck == snake)
-            {
-                Random rand2 = new Random();
-                int Dice = rand2.Next(1, 7);
-                Console.WriteLine("You went backward by:" + Dice);
+                if (count >= 0)
+                {
+                    Random random = new Random();
+                    int randomCheck = random.Next(3);
+
+                    switch (randomCheck)
+                    {
+                        case 1:
+                            {
+                                Random rnd = new Random();
+                                ldice = rnd.Next(1, 7);
+                                count = count + ldice;
+                                Console.WriteLine("Got Ladder");
+                                break;
+                            }
+                        case 2:
+                            {
+                                Random sran = new Random();
+                                sdice = sran.Next(1, 7);
+                                count = count - sdice;
+                                Console.WriteLine("Got snake");
+                                break;
+                            }
+                        default:
+                            {
+                                Console.WriteLine("No play");
+                                break;
+                            }
+                    }
+                }
+                else
+                {
+                    count = 0;
+                }
 
             }
-            else
-            {
-                Console.WriteLine("No play");
-            }
+            Console.WriteLine("Player wins" + count);
         }
     }
 }
+
