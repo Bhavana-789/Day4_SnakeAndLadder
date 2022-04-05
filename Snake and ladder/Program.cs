5d057b2f87e3ca0eeac9732595162b5b2f6baade
Bhavana <bhavananerlakota@gmail.com>
2022-04-06 04:24:39 +0530

    Added UC3 Check Option

--- Snake and ladder/Program.cs
+++ Snake and ladder/Program.cs
@@ -7,12 +7,32 @@ namespace Snake_and_ladder
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Snake and Ladder program");
-            //UC2
-            Console.WriteLine("Welcome to UC2 program");
+            //UC3
+            Console.WriteLine("Welcome to UC3 program");
+            Console.WriteLine("Welcome to UC3 program");
+            int Noplay = 0;
+            int ladder = 1;
+            int snake = 2;
             Random random = new Random();
-            int num = random.Next(1, 7);
-            Console.WriteLine("The Random Dice Number is: " + num);
+            int randomCheck = random.Next(1, 3);
+            Console.WriteLine("Checking for status" + randomCheck);
+            if (randomCheck == ladder)
+            {
+                Random random2 = new Random();
+                int Diceroll = random2.Next(1, 7);
+                Console.WriteLine("You have moved forward by:" + Diceroll);
+            }
+            else if (randomCheck == snake)
+            {
+                Random rand2 = new Random();
+                int Dice = rand2.Next(1, 7);
+                Console.WriteLine("You went backward by:" + Dice);
 
+            }
+            else
+            {
+                Console.WriteLine("No play");
+            }
         }
     }
 }
